a84a86d40259f82a27ee0b291f80f7016ff877b0
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-07-17 13:48:01 +0530

    Xumm donation added

---
+++ controllers/xummQRDonation.js
@@ -0,0 +1,52 @@
+const { XummSdk } = require("xumm-sdk");
+
+const { API_RESPONSE_CODE } = require("../constants/app.constants");
+
+
+const xummQRDonation = async (request, response) => {
+
+    const { body } = request;
+
+    if (!body) {
+        response.status(400).send({ error: API_RESPONSE_CODE[400] });
+        return;
+    };
+
+    try {
+
+        const Sdk = new XummSdk(process.env.XUMM_API_KEY, process.env.XUMM_API_SECRET)
+        const { amount, memo } = body;
+
+        if (!amount || amount.length === 0) {
+            response.status(400).send({ error: API_RESPONSE_CODE[400] });
+            return;
+        };
+
+        const request = {
+            "TransactionType": "Payment",
+            "Destination": process.env.DONATION_ADDRESS,
+            "Amount": amount,
+            "Memos": []
+        };
+
+        if (memo) {
+            request.Memos.push({
+                "Memo": {
+                    "MemoData": memo
+                }
+            });
+        };
+
+        const payload = await Sdk.payload.create(request, true);
+
+        response.status(200).send({
+            png: payload.refs.qr_png
+        });
+
+    } catch (err) {
+        console.log(err);
+        response.status(500).send({ error: API_RESPONSE_CODE[500] });
+    };
+};
+
+module.exports = xummQRDonation;
--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,12 @@ const fetchXrplAccountDetails = require("../controllers/fetchXrplAccountDetails"
 const saveAccountsList = require("../controllers/saveAccountsList");
 const deleteSavedAccount = require("../controllers/deleteSavedAccount");
 const fetchUserSavedAccounts = require("../controllers/fetchUserSavedAccounts");
+const xummQRDonation = require("../controllers/xummQRDonation");
 
 router.post("/save/accounts", decryptJSON, verifyJwt, saveAccountsList);
 router.post("/delete/account", decryptJSON, verifyJwt, deleteSavedAccount);
 router.post("/account/details", decryptJSON, verifyJwt, fetchXrplAccountDetails);
 router.post("/accounts", decryptJSON, verifyJwt, fetchUserSavedAccounts);
+router.post("/donate", xummQRDonation);
 
 module.exports = router;

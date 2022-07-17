fededc77982712d023c1bcd64cea6a6005803e22
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-07-17 21:01:52 +0530

    Error if account doesn't exist

--- controllers/storeAirdropDetails.js
+++ controllers/storeAirdropDetails.js
@@ -39,7 +39,10 @@ const storeAirdropDetails = async (request, response) => {
         const issuerAccountDetails = await client.request({
             command: "account_info",
             account: issuer,
-        });
+        }).catch(error => {
+            response.status(400).send({ error: error.data.error_message });
+            return;
+        })
 
         if (issuerAccountDetails.result) {
             const accountFlags = issuerAccountDetails.result.account_data.Flags;

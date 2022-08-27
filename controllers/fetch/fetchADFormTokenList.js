9d7add33451a2cb019cd32104cc77052bd034c26
Tushar Pardhe <tushar.pardhe@airtel.com>
2022-08-27 12:01:00 +0530

    Token list logic updated

--- controllers/fetch/fetchADFormTokenList.js
+++ controllers/fetch/fetchADFormTokenList.js
@@ -18,47 +18,50 @@ const fetchADFormTokenList = async (request, response) => {
             return response.status(400).send({ error: API_RESPONSE_CODE[400] });
         }
 
-        const existingADs = await Airdrop.find({ issuer: userFromDB.address });
+        const existingADs = await Airdrop.find();
 
         // List of issued tokens
         const client = new Client(process.env.XRPL_SERVER, { connectionTimeout: 10000 });
         await client.connect();
 
-        const gateway_balances = await client.request({
-            command: "gateway_balances",
-            account: userFromDB.address,
+        const account_lines = await client.request({
+            command: "account_lines",
             ledger_index: "validated",
+            limit: 52,
+            account: userFromDB.address,
         });
 
-        if (!gateway_balances?.result?.obligations || Object.keys(gateway_balances.result.obligations).length === 0) {
-            return response
-                .status(200)
-                .send({ currencies: [], message: "Token issuers can request for an airdrop listing. Please login using an issuer account." });
-        }
-
-        if (existingADs && existingADs.length > 0) {
-            existingADs.forEach((AD) => {
-                delete gateway_balances.result.obligations[AD.ticker];
-            });
+        if (account_lines.result.lines.length === 0) {
+            return response.status(200).send({ currencies: [], message: "No trustlines found.", listingFees: 5 });
         };
 
         const currencies = {};
-        Object.keys(gateway_balances.result.obligations).forEach((currency) => {
+        const drops = {};
+
+        if (existingADs && existingADs.length > 0) {
+            existingADs.forEach((AD) => {
+                drops[AD.ticker] = true;
+            });
+        };
+
+        account_lines.result.lines.map(({ currency, account, limit, balance }) => {
             let c = currency.length === 40 ? convertHexToString(currency).replaceAll("\u0000", "") : currency;
-            currencies[c] = {
-                issuer: userFromDB.address,
-                limit: gateway_balances.result.obligations[currency],
-                hex: currency
-            }
+            if (!drops[currency] && balance >= 0) {
+                currencies[c] = {
+                    issuer: account,
+                    limit: limit,
+                    hex: currency
+                }
+            };
         });
 
         if (!currencies || Object.keys(currencies).length === 0) {
-            return response.status(200).send({ currencies: [], message: "You've already submitted your requests." });
+            return response.status(200).send({ currencies: [], message: "You've already submitted your requests.", listingFees: 5 });
         }
 
-        const xrpl_currency_list = Object.keys(currencies).map((a) => ({ key: a, value: currencies[a].hex, text: a, limit: currencies[a].limit }));
+        const xrpl_currency_list = Object.keys(currencies).map((a) => ({ key: a, value: currencies[a].hex, text: a, limit: currencies[a].limit, issuer: currencies[a].issuer }));
 
-        response.status(200).send({ currencies: xrpl_currency_list, message: "" });
+        response.status(200).send({ currencies: xrpl_currency_list, message: "", listingFees: 5 });
         await client.disconnect();
         
     } catch (err) {
